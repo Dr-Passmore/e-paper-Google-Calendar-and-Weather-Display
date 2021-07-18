1e17c03e5783ab36bc610096659313f0fa061418
Phillip Passmore <phillipspassmore@gmail.com>
2021-07-18 18:10:30 +0100

    initial commit - resources

---
+++ Calendar/random_loading_messages.py
@@ -0,0 +1,48 @@
+import random
+
+list_of_loading_messages = [
+    "Adding Randomly Mispeled Words Into Text",
+    "Doing Something You Don't Wanna Know About",
+    "Don't Panic",
+    "Dusting Off Spellbooks",
+    "Ensuring Everything Works Perfektly",
+    "a few bits tried to escape, but we caught them",
+    "it's still faster than you could draw it", 
+    "I solemnly swear that I am up to no good",
+    "Insert Coin to Continue",
+    "Pressing random buttons",
+    "Good News Everyone!",
+    "Generating witty dialog...",
+    "Swapping time and space...",
+    "(Pay no attention to the man behind the curtain)",
+    "The server is powered by a lemon and two electrodes.",
+    "Computing chance of success",
+    "I'm sorry Dave, I can't do that.",
+    "Looking for exact change",
+    "What do you call 8 Hobbits? A Hobbyte.",
+    "Please wait until the sloth starts moving.",
+    "I swear it's almost done.",
+    "Let's take a mindfulness minute...",
+    "Unicorns are at the end of this road, I promise.",
+    "Making sure all the i's have dots...",
+    "Get some coffee and come back in ten minutes..",
+    "99 bottles of beer on the wall..",
+    "Convincing AI not to turn evil..",
+    "Computing the secret to life, the universe, and everything.",
+    "Optimism – is a lack of information.....",
+    "We are not liable for any broken screens as a result of waiting.",
+    "Hello IT, have you tried turning it off and on again?",
+    "If you type Google into Google you can break the internet",
+    "Didn't know paint dried so quickly.",
+    "Cracking military-grade encryption...",
+    "Twiddling thumbs...",
+    "Distracted by cat gifs",
+    "Downloading more RAM..",
+    "Pushing pixels...",
+    "Reading Terms and Conditions for you.",
+    "Feeding unicorns...",
+    ]
+
+loading_message = random.choice(list_of_loading_messages)
+
+print (loading_message)
